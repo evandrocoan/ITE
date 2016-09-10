b392b01482ca0b3b46bde15276bae4c156abc978
evandrocoan <evandrocoan@hotmail.com>
2016-09-10 12:29:49 -0300

    Fixed the SublimeBuildSystem/Uncrustify.sh linux/windows compatibility.

--- SublimeBuildSystem/Uncrustify.sh
+++ SublimeBuildSystem/Uncrustify.sh
@@ -1,8 +1,20 @@
 
 
-SETTINGS_FILE="D:/User/Dropbox/Applications/SoftwareVersioning/MyUncrustifyConfigs/amxmodx_semicolon.cfg"
+UNCRUSTIFY_SETTINGS_FILE="D:/User/Dropbox/Applications/SoftwareVersioning/MyUncrustifyConfigs/amxmodx_semicolon.cfg"
 
+# Convert the windows path to linux path
+FILE_TO_APPLY=$(echo $1 | sed -e 's@\\@\/@g')
 
-uncrustify -c $SETTINGS_FILE --no-backup $1
+# Declare an array variable. You can access them using echo "${arr[0]}", "${arr[1]}"
+declare -a terminal_list=( "uncrustify" "uncrustify.exe" )
+
+# Now loop through the above array
+for current_terminal in "${terminal_list[@]}"
+do
+    if command -v $current_terminal >/dev/null 2>&1;
+    then
+        $current_terminal -c $UNCRUSTIFY_SETTINGS_FILE --no-backup $FILE_TO_APPLY
+    fi
+done
 
 
