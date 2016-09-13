b0b1708f72c384a6282de54fe43811d27f27f230
evandrocoan <evandrocoan@hotmail.com>
2016-09-12 21:40:45 -0300

    Fixed the global 'isNotSyncedSideBarEnabled' not updating on SublimeDefaultSyntax/default_syntax.py.

--- SublimeDefaultSyntax/default_syntax.py
+++ SublimeDefaultSyntax/default_syntax.py
@@ -8,6 +8,7 @@ import sublime_plugin
 class DefaultSyntaxCommand(sublime_plugin.EventListener):
 
 	def on_new(self, view):
+
 		view.set_syntax_file("Packages/C++/C++.tmLanguage")
 
 
@@ -15,6 +16,7 @@ class DefaultSyntaxCommand(sublime_plugin.EventListener):
 class ForceRewriteSublimeSettingsCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
+
         sublime.save_settings('Preferences.sublime-settings')
 
 
@@ -23,41 +25,55 @@ isNotSyncedSideBarEnabled = True
 
 class SyncedSideBarRevealInSideBarCommand(sublime_plugin.TextCommand):
 
+    global isNotSyncedSideBarEnabled
+
     def run(self, edit):
 
-        self.view.window ().run_command ("reveal_in_side_bar")
+        self.view.window().run_command ("reveal_in_side_bar")
 
 
-    def is_visible(self, event):
+    def is_visible(self):
 
+        print( 'isNotSyncedSideBarEnabled: ' + str( isNotSyncedSideBarEnabled ) )
         return isNotSyncedSideBarEnabled
 
 
 def plugin_loaded():
+
+    global isNotSyncedSideBarEnabled
 
     packageControlSettings = sublime.load_settings('Package Control.sublime-settings')
     userSettings           = sublime.load_settings('Preferences.sublime-settings')
 
     def updateIsSyncedSideBarEnabled():
 
+        global isNotSyncedSideBarEnabled
         print('    updateIsSyncedSideBarEnabled!!!!')
 
         ignoredPackages   = userSettings.get( "ignored_packages" )
         installedPackages = packageControlSettings.get( "installed_packages" )
+        isIgnored         = any( "SyncedSideBar" in package for package in ignoredPackages )
+        isInstalled       = any( "SyncedSideBar" in package for package in installedPackages )
+        
+        print( 'isIgnored: ' + str( isIgnored ) )
+        print( 'isInstalled: ' + str( isInstalled ) )
 
-        if any( "SyncedSideBar" in package for package in installedPackages ):
+        if isIgnored:
 
-            if any( "SyncedSideBar" in package for package in ignoredPackages ):
-
-                isNotSyncedSideBarEnabled = True
-
-            else:
-
-                isNotSyncedSideBarEnabled = False
+            isNotSyncedSideBarEnabled = True
 
         else:
 
-            isNotSyncedSideBarEnabled = True
+            if isInstalled:
+
+                isNotSyncedSideBarEnabled = False
+
+            else:
+
+                isNotSyncedSideBarEnabled = True
+
+        print( 'isNotSyncedSideBarEnabled: ' + str( isNotSyncedSideBarEnabled ) )
+
 
 
     def read_pref_async():
@@ -87,9 +103,6 @@ def plugin_loaded():
 
     #print( userSettings.get( "ignored_packages" ) )
     #print( packageControlSettings.get( "installed_packages" ) )
-
-
-
 
 
 
