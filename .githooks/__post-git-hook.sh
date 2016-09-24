0d0a4bcaf433d28740ce1fb3da60ce96bf138e86
evandrocoan <evandrocoan@hotmail.com>
2016-09-24 15:37:06 -0300

    Changed the auto-versioning trigger branch from develop to master

--- .githooks/__post-git-hook.sh
+++ .githooks/__post-git-hook.sh
@@ -26,13 +26,13 @@ cleanUpdateFlagFile()
 }
 
 
-# Updates and changes the files if the flag file exits, if and only if we are on the 'develop'
+# Updates and changes the files if the flag file exits, if and only if we are on the 'master'
 # branch.
 # '-C HEAD' do not prompt for a commit message, use the HEAD as commit message.
 # '--no-verify' do not call the pre-commit hook to avoid infinity loop.
 if [ -f $updateFlagFilePath ]
 then
-    if [[ $currentBranch == "develop" ]]
+    if [[ $currentBranch == "master" ]]
     then
         if sh $updateVersionProgram build
         then
@@ -45,7 +45,7 @@ then
         echo "Amending commits..."
         git commit --amend -C HEAD --no-verify
     else
-        echo "It is not time to amend, as we are not on the 'develop' branch."
+        echo "It is not time to amend, as we are not on the 'master' branch."
     fi
 else
     echo "It is not time to amend, as the file '$updateFlagFilePath' does not exist."
