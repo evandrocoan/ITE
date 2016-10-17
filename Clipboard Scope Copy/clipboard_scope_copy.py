fec0f92ab89ef0ffe58ebc8644daf84e9c90014c
evandrocoan <evandrocoan@hotmail.com>
2016-10-17 19:34:01 -0200

    Fixed mixed spaces/tabs on clipboard_scope_copy.py

--- Clipboard Scope Copy/clipboard_scope_copy.py
+++ Clipboard Scope Copy/clipboard_scope_copy.py
@@ -4,11 +4,13 @@ import sublime_plugin
 
 
 class CopyScopeToClipboardCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    syntax_name = self.view.scope_name(self.view.sel()[0].begin())
-    print(syntax_name)
-    self.view.set_status("Scope",syntax_name)
-    sublime.set_clipboard(syntax_name)
+
+    def run(self, edit):
+
+        syntax_name = self.view.scope_name(self.view.sel()[0].begin())
+        print(syntax_name)
+        self.view.set_status("Scope",syntax_name)
+        sublime.set_clipboard(syntax_name)
 
 
 
@@ -19,10 +21,10 @@ class GetScopeAlwaysTextCommand( sublime_plugin.TextCommand ):
     `Toggle ScopeAlways` in order to this to work.
     """
 
-	def run( self, edit ):
+    def run( self, edit ):
 
-		# status = self.view.get_status( "scope_always" )
-		status = sublime.active_window().active_view().get_status( "scope_always" )
-		print( status )
+        # status = self.view.get_status( "scope_always" )
+        status = sublime.active_window().active_view().get_status( "scope_always" )
+        print( status )
 
 
