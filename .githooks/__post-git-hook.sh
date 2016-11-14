9f178e25860fc7561cf3d0d0fb693ac2bb9779d4
evandrocoan <evandrocoan@hotmail.com>
2016-11-14 14:50:00 -0200

    Fixed the prepare-commit-msg not working for all/any branches.

--- .githooks/__post-git-hook.sh
+++ .githooks/__post-git-hook.sh
@@ -39,8 +39,6 @@ cleanUpdateFlagFile()
 
 # Updates and changes the files if the flag file exits, if and only if we are on the '$targetBranch'
 # branch.
-# '-C HEAD' do not prompt for a commit message, use the HEAD as commit message.
-# '--no-verify' do not call the pre-commit hook to avoid infinity loop.
 if [ -f $updateFlagFilePath ]
 then
     if [[ $currentBranch == $targetBranch || $targetBranch == "." ]]
@@ -53,6 +51,9 @@ then
             cleanUpdateFlagFile
             exit 1
         fi
+
+        # '-C HEAD' do not prompt for a commit message, use the HEAD as commit message.
+        # '--no-verify' do not call the pre-commit hook to avoid infinity loop.
         printf "Amending commits...\n"
         git commit --amend -C HEAD --no-verify
     else
