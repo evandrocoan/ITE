4c1dcd2bfdcfa21abec4ee34c98a6f9e118debd3
evandrocoan <evandrocoan@hotmail.com>
2016-11-22 16:42:56 -0200

    Set the setting "scroll_past_end": false and finished the issue:
    https://github.com/evandrocoan/SublimeTextStudio/issues/26
    
    First part implementation. Now remains to replace:
    ```
            view.run_command( "move", {"by": "lines", "forward": False} )
            view.run_command( "move", {"by": "lines", "forward": True} )
    ```
    By a actual command to change the view to the current cursor
    centered position.

--- Fix Project Switch-Restart Bug/fix_project_switch_restart_bug.py
+++ Fix Project Switch-Restart Bug/fix_project_switch_restart_bug.py
@@ -5,6 +5,17 @@ import sublime
 import sublime_plugin
 
 
+def plugin_loaded():
+
+    views = sublime.active_window().views()
+
+    for view in views:
+
+        # print( "( plugin_loaded ) Callling on_activated, view id {0}".format( view.id() ) )
+        view.run_command( "move", {"by": "lines", "forward": False} )
+        view.run_command( "move", {"by": "lines", "forward": True} )
+
+
 class OnLoadedViewCommand( sublime_plugin.EventListener ):
     """
         This partially fix the issues:
@@ -15,21 +26,16 @@ class OnLoadedViewCommand( sublime_plugin.EventListener ):
         Scroll sync doesn't work (ST3 3084)
         https://github.com/titoBouzout/BufferScroll/issues/23
 
-        To accomplish it, it move the caret up and down, this way it forces the scroll view to be switched.
-        This causes the undesired behavior when switching views after the file being loaded or the
-        project being restored.
-
-        To fix this problems, just on to perform those operations when the file is loaded for the first time.
-        This is a problem when Sublime Text just starts because this package is not loaded yet. This way
-        we need to perform this action at the first time the file is chose/switched to.
+        Finish the package: Fix Project Switch-Restart Bug
+        https://github.com/evandrocoan/SublimeTextStudio/issues/26
     """
 
-    # def on_load( self, view ):
+    def on_load( self, view ):
     # def on_load_async( self, view ):
-    def on_activated( self, view ):
+    # def on_activated( self, view ):
     # def on_activated_async( self, view ):
 
-        # print( "( OnLoadedViewCommand ) Callling on_activated" )
+        # print( "( OnLoadedViewCommand ) Callling on_activated, view id {0}".format( view.id() ) )
         # { "keys": ["up"], "command": "move", "args": {"by": "lines", "forward": false} },
         # { "keys": ["down"], "command": "move", "args": {"by": "lines", "forward": true} },
         view.run_command( "move", {"by": "lines", "forward": False} )
