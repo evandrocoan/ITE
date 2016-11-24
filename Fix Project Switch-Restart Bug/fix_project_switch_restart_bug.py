d31bf424fb45185210d02e1e966cd0224f2f9d23
evandrocoan <evandrocoan@hotmail.com>
2016-11-24 07:33:16 -0200

    Finished the Fix Project Switch-Restart Bug main structure and doc.

--- Fix Project Switch-Restart Bug/fix_project_switch_restart_bug.py
+++ Fix Project Switch-Restart Bug/fix_project_switch_restart_bug.py
@@ -7,6 +7,8 @@ import sublime_plugin
 
 def plugin_loaded():
 
+    # print( "( fix_project_switch_restart_bug.py )" )
+
     views   = None
     windows = sublime.windows()
 
@@ -16,9 +18,40 @@ def plugin_loaded():
 
         for view in views:
 
-            print( "( plugin_loaded ) Callling on_activated, view id {0}".format( view.id() ) )
-            view.run_command( "move", {"by": "lines", "forward": False} )
-            view.run_command( "move", {"by": "lines", "forward": True} )
+            # print( "( fix_project_switch_restart_bug.py ) View id {0}, buffer id {1}".format( view.id(), view.buffer_id() ) )
+            restore_view( view )
+
+
+
+def restore_view( view ):
+    """
+        view.set_viewport_position( , False )
+    """
+
+    # for selection in view.sel(): print( "( fix_project_switch_restart_bug.py ) Iterating view.sel()[i].begin() {0}".format( selection ) )
+
+    # print( "( fix_project_switch_restart_bug.py ) Setting show_at_center to view id {0}".format( view.id() ) )
+    view.show_at_center( view.sel()[0].begin() )
+
+
+
+lastOnLoadViewCallTime = 0
+
+def are_we_on_the_project_switch_process():
+    """
+        Call `plugin_loaded()` only one time, after the project switch process is finished.
+
+        Restrictions:
+        1. We cannot call `plugin_loaded()` if this function is called only one time.
+        2. If this function is called consequently 2 times with less than 0.1 seconds, then we
+           must to return True.
+           Basically, on each call
+    """
+
+    # set task to call plugin_loaded() after 2 seconds, if this task currently does not exists.
+
+    return False
+
 
 
 class OnLoadedViewCommand( sublime_plugin.EventListener ):
@@ -35,16 +68,36 @@ class OnLoadedViewCommand( sublime_plugin.EventListener ):
         https://github.com/evandrocoan/SublimeTextStudio/issues/26
     """
 
-    def on_load( self, view ):
-    # def on_load_async( self, view ):
+    # def on_load( self, view ):
+    def on_load_async( self, view ):
     # def on_activated( self, view ):
     # def on_activated_async( self, view ):
 
-        # print( "( OnLoadedViewCommand ) Callling on_activated, view id {0}".format( view.id() ) )
         # { "keys": ["up"], "command": "move", "args": {"by": "lines", "forward": false} },
         # { "keys": ["down"], "command": "move", "args": {"by": "lines", "forward": true} },
-        view.run_command( "move", {"by": "lines", "forward": False} )
-        view.run_command( "move", {"by": "lines", "forward": True} )
+        # view.run_command( "move", {"by": "lines", "forward": False} )
+        # view.run_command( "move", {"by": "lines", "forward": True} )
+        #
+        # print( "( fix_project_switch_restart_bug.py ) Calling restore_view, view id {0}".format( view.id() ) )
+
+        # Need to fix to the bug on the Sublime Text core, where all cloned views are called with the same
+        # view object. If we do not fix this here, it would set all the cloned views to the same
+        # position. So to overcome this, we call all the views to center with `plugin_loaded()`.
+        #
+        # #1253 Event handlers for cloned views get called with the wrong view object
+        # https://github.com/SublimeTextIssues/Core/issues/1253
+        #
+        # The `on_load_async()` is called for each view on for the project change, however
+        # due the bug https://github.com/SublimeTextIssues/Core/issues/1253, the cloned
+        # views are called with the wrong `view`, then we need to call `plugin_loaded()` to catch
+        # all the correct views (including the cloned ones) to apply the scroll fix for the bug
+        # https://github.com/SublimeTextIssues/Core/issues/1379.
+        #
+        if not are_we_on_the_project_switch_process():
+
+            restore_view( view )
+
+
 
 
 
