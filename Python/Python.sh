33dce39328db6499f2401bdd5a2b1c06988ddc5d
evandrocoan <evandrocoan@hotmail.com>
2016-12-01 00:23:15 -0200

    Created the package Python/Python.sublime-build to override the
    default Python build system within a shell script one.

---
+++ Python/Python.sh
@@ -0,0 +1,8 @@
+
+
+# $1 is the first shell argument passed by SQL.sublime-build, usually it should be the SQL file path.
+#
+# "C:\Program Files (x86)\PostgreSQL\9.6\bin\psql.exe" --host "localhost" --port 5432 --username "postgres" -d "aula20"
+#
+python $1
+
