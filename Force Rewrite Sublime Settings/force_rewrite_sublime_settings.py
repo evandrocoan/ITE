0c99b73951e82ac5b57d30f2b31d850ada25295d
evandrocoan <evandrocoan@hotmail.com>
2017-02-05 14:16:16 -0200

    Allowed the Force Rewrite Sublime Settings to reload all views once.

--- Force Rewrite Sublime Settings/force_rewrite_sublime_settings.py
+++ Force Rewrite Sublime Settings/force_rewrite_sublime_settings.py
@@ -15,8 +15,18 @@ class ForceReloadSublimeColorScheme( sublime_plugin.TextCommand ):
 
     def run( self, edit ):
 
-        print( "\n" + self.view.window().active_view().settings().get("color_scheme") )
-        self.view.window().active_view().settings().erase("color_scheme")
+        views   = None
+        windows = sublime.windows()
+
+        for window in windows:
+
+            print( "\nWindow id: " + str( window.id() ) )
+            views = window.views()
+
+            for view in views:
+
+                print( self.view.window().active_view().settings().get("color_scheme") )
+                view.settings().erase("color_scheme")
 
 
 
