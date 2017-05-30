284e82abda1088587a105f695eeb7f9f807bea6c
evandrocoan <evandrocoan@hotmail.com>
2017-05-30 15:53:08 -0300

    Fixed the the overwrite_commit_completion.py variables naming.

--- Overwrite Commit Completion/overwrite_commit_completion.py
+++ Overwrite Commit Completion/overwrite_commit_completion.py
@@ -14,14 +14,24 @@ class OverwriteCommitCompletionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         window = view.window()
-        window.run_command("commit_completion")
+        window.run_command( "commit_completion" )
 
         for sel in view.sel():
-            point = sel.b
-            word = view.word(point)
-            reg = sublime.Region(point, word.end())
+            completion_end_point   = sel.b
+            sublime_completed_word = view.word( completion_end_point )
+            duplicated_word_region = sublime.Region( completion_end_point, sublime_completed_word.end() )
 
-            if view.substr(reg).isalnum():
-                view.erase(edit, reg)
+            # print( "completion_end_point:   " + str( completion_end_point ) )
+            # print( "sublime_completed_word: " + view.substr( sublime_completed_word ) )
+            # print( "duplicated_word_region: " + view.substr( duplicated_word_region ) )
 
+            if view.substr( duplicated_word_region ).isalnum():
+                # print( "Erasing duplicated word: " + view.substr( duplicated_word_region ) )
+                view.erase( edit, duplicated_word_region )
 
+""" OverwriteCommitCompletionCommand
+
+OverwriteCommitCompletionCommand
+OverwriteCommitCompletionCommand
+
+"""
