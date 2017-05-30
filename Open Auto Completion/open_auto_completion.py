4523f5f0946f32e74ebf04df6d0dc3d0d2235c75
evandrocoan <evandrocoan@hotmail.com>
2017-05-29 23:09:34 -0300

    Reverted the <character> due wanted auto completion while
    typing symbols as ., ?, etc.
    
    Issue:
    https://github.com/SublimeTextIssues/Core/issues/1738

--- Open Auto Completion/open_auto_completion.py
+++ Open Auto Completion/open_auto_completion.py
@@ -14,11 +14,11 @@ class OpenAutoCompletionCommand(sublime_plugin.TextCommand):
         https://forum.sublimetext.com/t/run-command-on-space/28198
     """
 
-    def run(self, edit, character):
+    def run(self, edit, **kargs):
         # print( "kargs: ", str( kargs ) )
 
         view = self.view
-        view.run_command("insert", {"characters": character})
+        view.run_command("insert", {"characters": kargs["keystroke"]})
 
         window = view.window()
         window.run_command("auto_complete", {'disable_auto_insert': True, 'next_completion_if_showing': False})
