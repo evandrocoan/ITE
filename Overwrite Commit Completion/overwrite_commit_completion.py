3a3e0c3a742af319beb88dcb41ad0f9a30bfc20f
evandrocoan <evandrocoan@hotmail.com>
2017-05-31 11:37:29 -0300

    Wrote a variation for the Overwrite Commit Completion where it only
    replaces the remaining word, if it is a subword of the completed
    word. I added this because most times I am adding new words before
    the some word, and when I use the completion for this new word it
    was deleting my word.

--- Overwrite Commit Completion/overwrite_commit_completion.py
+++ Overwrite Commit Completion/overwrite_commit_completion.py
@@ -14,20 +14,44 @@ class OverwriteCommitCompletionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         view = self.view
         window = view.window()
+        old_selections = []
+
+        for selection in view.sel():
+            old_selections.append( selection.end() )
+
+        selection_index = 0
+        completion_offset = 0
         window.run_command( "commit_completion" )
 
-        for sel in view.sel():
-            completion_end_point   = sel.b
-            sublime_completed_word = view.word( completion_end_point )
-            duplicated_word_region = sublime.Region( completion_end_point, sublime_completed_word.end() )
+        for selection in view.sel():
+            completion_end_point   = selection.end()
+            duplicated_word_region = sublime.Region( completion_end_point, view.word( completion_end_point ).end() )
+            duplicated_word        = view.substr( duplicated_word_region )
+            part_completed_region  = sublime.Region( old_selections[ selection_index ] + completion_offset, completion_end_point )
 
-            # print( "completion_end_point:   " + str( completion_end_point ) )
-            # print( "sublime_completed_word: " + view.substr( sublime_completed_word ) )
-            # print( "duplicated_word_region: " + view.substr( duplicated_word_region ) )
+            # print( "selection:           " + str( selection ) )
+            # print( "inserted_word:       " + view.substr( sublime.Region( view.word( completion_end_point ).begin(), completion_end_point ) ) )
+            # print( "full_completed_word: " + view.substr( view.word( completion_end_point ) ) )
+            # print( "duplicated_word:     " + duplicated_word )
+            # print( "part_completed_word: " + view.substr( part_completed_region ) )
 
-            if view.substr( duplicated_word_region ).isalnum():
-                # print( "Erasing duplicated word: " + view.substr( duplicated_word_region ) )
+            # inserted_word:       OverwriteCommitCompletionCommand
+            # full_completed_word: OverwriteCommitCompletionCommandCommand
+            # duplicated_word:     Command
+            # part_completed_word: ompletionCommand
+            if duplicated_word.isalnum() \
+                    and duplicated_word in view.substr( part_completed_region ):
+
+                # print( "Erasing duplication: " + duplicated_word )
                 view.erase( edit, duplicated_word_region )
+
+                # When the completion is inserted we need to save the completion_offset to be able
+                # correct the outdated selection points after the auto completion for the remaining
+                # selections
+                completion_offset += part_completed_region.size() - duplicated_word_region.size()
+
+            selection_index += 1
+
 
 """ OverwriteCommitCompletionCommand
 
