a64317ddc24829e0e5e22b905a1baec661be75cd
evandrocoan <evandrocoan@hotmail.com>
2017-06-17 17:34:17 -0300

    Created the Sublime Text Studio/channel.json example file.

---
+++ Sublime Text Studio/sublime_text_studio.py
@@ -0,0 +1,15 @@
+
+
+import os
+
+import sublime
+import sublime_plugin
+
+
+class SublimeTextStudioGenerateChannelFileCommand( sublime_plugin.TextCommand ):
+
+    def run( self, edit ):
+
+        print( 'Calling SublimeTextStudioGenerateChannelFile...' )
+
+
