7369533bbe32c25ce8f2c6b27e008621ed62fc37
evandrocoan <evandrocoan@hotmail.com>
2017-06-18 23:22:24 -0300

    Removed all sublimehq/Packages files to clean the working tree
    and remove them from the main repository folder, splitting it
    from the sublimehq/Packages files, on migration to the
    `Packages/` folder.

---
+++ Packages/User/Java/Java.sh
@@ -0,0 +1,10 @@
+
+
+# Uncomment this to build all java file. This assumes the current working directory is correctly set.
+#usr/bin/find *.java -exec javac -encoding UTF8 {} \;
+
+
+javac $1
+java $2
+
+
