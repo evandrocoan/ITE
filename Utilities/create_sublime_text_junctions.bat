7a3238c9137138112c453ef1756ec1c52b649e39
evandrocoan <evandrocoan@hotmail.com>
2018-08-12 15:13:29 -0300

    Created the Utilities/create_sublime_text_junctions.bat

---
+++ Utilities/create_sublime_text_junctions.bat
@@ -0,0 +1,11 @@
+
+
+pause
+
+mkdir "F:\SublimeText\MSYS2\Data\"
+
+mklink /J "F:\SublimeText\MSYS2\Data\Installed Packages" "F:\SublimeText\Data\Installed Packages"
+mklink /J "F:\SublimeText\MSYS2\Data\Lib" "F:\SublimeText\Data\Lib"
+mklink /J "F:\SublimeText\MSYS2\Data\Packages" "F:\SublimeText\Data\Packages"
+
+pause
