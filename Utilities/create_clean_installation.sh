5ef36ef4337c0244371db36e2a9aa001d7ba12b1
evandrocoan <evandrocoan@hotmail.com>
2018-10-17 22:47:50 -0300

    Fixed the create_clean_installation.sh command line being overridden

--- Utilities/create_clean_installation.sh
+++ Utilities/create_clean_installation.sh
@@ -5,15 +5,17 @@ target_zip_file="$1"
 build="$2"
 
 if [ -z $target_zip_file ]; then
-    target_zip_file="IteBasicStudioTest.zip"
+    target_zip_file="PackageControlBuilt.zip"
+
+    # sync_only="Uncomment this to sync only, instead of doing a full replacement."
+    # target_zip_file="NothingBuilt.zip"
+    # target_zip_file="PackageControlBuilt.zip"
+    target_zip_file="PackagesManagerBuild.zip"
 fi
 
 if [ -z $build ]; then
     build="3176"
 fi
-
-# sync_only="Uncomment this to sync only, instead of doing a full replacement."
-# target_zip_file="IteMinimal.zip"
 
 full_data="./../"
 clean_testing="./../../CleanTesting"
@@ -35,7 +37,7 @@ fi
 printf "Unzipping files from %s\n" "$(get_file_path "$clean_testing/$target_zip_file")"
 unzip -q -n "./$clean_testing/$target_zip_file" -d "$build/Data/"
 
-if [[ "$target_zip_file" != "IteMinimal.zip" ]]
+if [[ "$target_zip_file" != "NothingBuilt.zip" ]]
 then
     printf "\n"
     printf "Syncing folders...\n"
@@ -60,15 +62,35 @@ then
     rsync -r --exclude /.git "$full_data/Packages/DebugTools/" \
             "$build/Data/Packages/DebugTools/"
 
-    # printf "%s\n" "$(get_file_path "$build/Data/Packages/PackagesManager")"
-    # mkdir -p "$build/Data/Packages/PackagesManager"
-    # rsync -r --exclude /.git "$full_data/Packages/PackagesManager/" \
-    #         "$build/Data/Packages/PackagesManager/"
+    printf "%s\n" "$(get_file_path "$build/Data/Packages/PackagesManager")"
+    mkdir -p "$build/Data/Packages/PackagesManager"
+    rsync -r --exclude /.git "$full_data/Packages/PackagesManager/" \
+            "$build/Data/Packages/PackagesManager/"
 
     printf "%s\n" "$(get_file_path "$build/Data/Packages/UnitTesting")"
     mkdir -p "$build/Data/Packages/UnitTesting"
     rsync -r --exclude /.git "$full_data/Packages/UnitTesting/" \
             "$build/Data/Packages/UnitTesting/"
+
+    printf "%s\n" "$(get_file_path "$build/Data/Packages/coverage")"
+    mkdir -p "$build/Data/Packages/coverage"
+    rsync -r --exclude /.git "$full_data/Packages/coverage/" \
+            "$build/Data/Packages/coverage/"
+
+    printf "%s\n" "$(get_file_path "$build/Data/Packages/ConcurrentLogHandler")"
+    mkdir -p "$build/Data/Packages/ConcurrentLogHandler"
+    rsync -r --exclude /.git "$full_data/Packages/ConcurrentLogHandler/" \
+            "$build/Data/Packages/ConcurrentLogHandler/"
+
+    printf "%s\n" "$(get_file_path "$build/Data/Packages/PortalockerFiles")"
+    mkdir -p "$build/Data/Packages/PortalockerFiles"
+    rsync -r --exclude /.git "$full_data/Packages/PortalockerFiles/" \
+            "$build/Data/Packages/PortalockerFiles/"
+
+    printf "%s\n" "$(get_file_path "$build/Data/Packages/python-pywin32")"
+    mkdir -p "$build/Data/Packages/python-pywin32"
+    rsync -r --exclude /.git "$full_data/Packages/python-pywin32/" \
+            "$build/Data/Packages/python-pywin32/"
 
 fi
 
