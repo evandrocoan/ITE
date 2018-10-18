3fec74625afd13412d2fb85f9604a9b9a145ad8b
evandrocoan <evandrocoan@hotmail.com>
2018-10-18 19:05:55 -0300

    Created the Utilities/update_stable_installation.sh script

---
+++ Utilities/update_stable_installation.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+printf "$(date)\n"
+
+
+full_data="./../"
+stable_installation="./../../StableVersion"
+
+rm -r -f "$stable_installation/Data/Packages/User/PackagesManager.cache"
+rm -r -f "$stable_installation/Data/Packages/User/PackagesManager.last-run"
+
+get_file_path () {
+   printf "%s" "$(cd $(dirname "$1") && pwd -P)/$(basename "$1")"
+}
+
+get_directory_path () {
+   printf "%s" "$(cd $(dirname "$1") && pwd -P)/"
+}
+
+sync_directory_path () {
+    package_name="$1"
+    printf "%s\n" "$(get_file_path "$stable_installation/Data/Packages/$package_name")"
+    mkdir -p "$stable_installation/Data/Packages/$package_name"
+    rsync -r --exclude /.git --exclude __pycache__ "$full_data/Packages/$package_name/" \
+            "$stable_installation/Data/Packages/$package_name/"
+}
+
+printf "\n"
+printf "Syncing folders...\n"
+
+# sync_directory_path "StudioChannel"
+# sync_directory_path "ChannelManager"
+sync_directory_path "PackagesManager"
+
+printf "\n"
+printf "Done syncing!\n"
