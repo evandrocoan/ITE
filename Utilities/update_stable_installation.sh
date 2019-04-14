f26a7143d6bc0f6b3015ef714c0f59cf2a23e925
evandrocoan <evandrocoan@hotmail.com>
2019-04-14 19:18:37 -0300

    Removed extra space on update_stable_installation.sh

--- Utilities/update_stable_installation.sh
+++ Utilities/update_stable_installation.sh
@@ -8,15 +8,15 @@ stable_installation="./../../StableVersion"
 rm -r -f "$stable_installation/Data/Packages/User/PackagesManager.cache"
 rm -r -f "$stable_installation/Data/Packages/User/PackagesManager.last-run"
 
-get_file_path () {
+get_file_path() {
    printf "%s" "$(cd $(dirname "$1") && pwd -P)/$(basename "$1")"
 }
 
-get_directory_path () {
+get_directory_path() {
    printf "%s" "$(cd $(dirname "$1") && pwd -P)/"
 }
 
-sync_directory_path () {
+sync_directory_path() {
     package_name="$1"
     printf "%s\n" "$(get_file_path "$stable_installation/Data/Packages/$package_name")"
     mkdir -p "$stable_installation/Data/Packages/$package_name"
